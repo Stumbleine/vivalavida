d68c3905fa9b1487b3bca5f968d94c93ed22d3d3
johann.geisser <johann.geisser@training.assuresoft.com>
2023-02-20 15:55:23 -0400

    Adding Support to save assets in corresponding upload of images

--- src/pages/public/RegisterSong.jsx
+++ src/pages/public/RegisterSong.jsx
@@ -1,10 +1,13 @@
 import { Button, Stack, TextField } from '@mui/material';
 import { Form, FormikProvider, useFormik } from 'formik';
-import React from 'react';
+import {useState} from 'react';
 import * as Yup from 'yup';
 
 
 export default function AlbumForm() {
+  
+  const [selectedFile, setSelectedFile] = useState(null);
+
   const formik = useFormik({
     initialValues: {
       title: '',
@@ -19,15 +22,31 @@ export default function AlbumForm() {
       title: Yup.string().required("El titulo es requerido"),
       gender: Yup.string().required("El genero es requerido"),
       artist: Yup.string().required("El artista es requerido"),
-      album: Yup.string().required("El album es requerido"),
+      // album: Yup.string().required("El album es requerido"),
       duration: Yup.string().required("La duracion es requerida"),
       linkMusic: Yup.string().required("El link es requerido")
     }),
     onSubmit: values => {
-
+      console.log('Inside on SUBMIT');
+      const add = async () => {
+        const formData = new FormData();
+        formData.append('image', selectedFile);
+				await fetch('http://localhost:4000/api/artist/image', {
+        method: 'POST',
+        body: formData,
+      });
+			};
+			add();
     },
   });
   const { getFieldProps, values, errors, touched, isSubmitting } = formik;
+
+
+  const handleFileChange = (event, setFieldValue) => {
+    console.log(event.target.files[0]);
+    setSelectedFile(event.target.files[0]);
+  };
+
   return (
     <FormikProvider value={formik}>
       <Form autoComplete>
@@ -53,12 +72,27 @@ export default function AlbumForm() {
             error={Boolean(touched.artist && errors.artist)}
             helperText={touched.artist && errors.artist}
           />
-          <TextField
+          {/* <TextField
             fullWidth
             label="Album"
             {...getFieldProps('album')}
             error={Boolean(touched.album && errors.album)}
             helperText={touched.album && errors.album}
+          /> */}
+           {/* <TextField
+            fullWidth
+            type="file"
+            label="Album"
+            onChange={handleFileChange}
+            helperText={touched.album && errors.album}
+          /> */}
+          <input
+            type="file"
+            label="Album"
+            onChange={(event) => {
+              // formik.setFieldValue("image", event.currentTarget.files[0]);
+              handleFileChange(event)
+            }}
           />
           <TextField
             label="Duracion"
